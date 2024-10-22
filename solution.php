157df880f681228fe284caff76f0c9f43727c735
ToulAura <102886021+M1cki0g@users.noreply.github.com>
2024-10-22 20:42:31 +0100

    Create solution.php

---
+++ solution.php
@@ -0,0 +1,13 @@
+
+    <?php
+    echo "<table style='border: 1px solid; '>";
+    for ($i = 1; $i <= 3; $i++) {
+        echo "<tr>";
+        for ($j = 1; $j <= 3; $j++) {
+            echo "<td style='border: 1px solid';>$i$j</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+
+    ?>
